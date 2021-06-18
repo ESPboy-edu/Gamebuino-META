b0414e27165bb3e32859d446301ff367047dc68d
ESPboy <ESPboy.edu@gmail.com>
2021-06-18 20:27:29 +0300

    fixed configs

--- src/config/config-default.h
+++ src/config/config-default.h
@@ -1,48 +1,3 @@
-#ifndef FOLDER_NAME
-#ifndef __SKETCH_NAME__
-#define FOLDER_NAME "testsketch"
-#endif
-#endif
-
-#ifndef NO_ARDUINO
-#define NO_ARDUINO 0
-#endif
-
-#ifndef USE_SDFAT
-#define USE_SDFAT 1
-#endif
-
-#ifndef CUSTOM_TFT_FUNCTIONS
-#define CUSTOM_TFT_FUNCTIONS 0
-#endif
-
-#ifndef CUSTOM_TFT_SPI_FUNCTIONS
-#define CUSTOM_TFT_SPI_FUNCTIONS 0
-#endif
-
-#ifndef USE_TFT_DESCRIPTORS
-#define USE_TFT_DESCRIPTORS 1
-#endif
-
-#ifndef CUSTOM_BUTTON_FUNCTIONS
-#define CUSTOM_BUTTON_FUNCTIONS 1
-#endif
-
-#ifndef USE_PRINTF
-#define USE_PRINTF 0
-#endif
-
-#ifndef CUSTOM_MALLOC
-#define CUSTOM_MALLOC 0
-#endif
-
-#ifndef AUTOCREATE_OBJECT
-#define AUTOCREATE_OBJECT 1
-#endif
-
-#ifndef NEOPIXELS_ALTERNATIVE_METHOD
-#define NEOPIXELS_ALTERNATIVE_METHOD 0
-#endif
 
 ///////////////
 // save defines
@@ -52,21 +7,6 @@
 #define SAVEBLOCK_NUM 256
 #endif
 
-#ifndef SAVEFILE_NAME
-#define SAVEFILE_NAME "/SAVE.SAV"
-#endif
-
-#ifndef SAVECONF_DEFAULT_BLOBSIZE
-#define SAVECONF_DEFAULT_BLOBSIZE 32
-#endif
-
-#ifndef AUTOSHOW_TITLESCREEN
-#define AUTOSHOW_TITLESCREEN 0
-#endif
-
-#ifndef AUTOSHOW_STARTSCREEN
-#define AUTOSHOW_STARTSCREEN 1
-#endif
 
 ///////////////
 // display defines
@@ -92,37 +32,7 @@
 #endif
 #endif
 
-///////////////
-// sound defines
-///////////////
 
-#ifndef SOUND_CHANNELS
-#define SOUND_CHANNELS 4
-#endif
-
-#ifndef SOUND_FREQ
-#define SOUND_FREQ 44100
-#endif
-
-#ifndef SOUND_BUFFERSIZE
-#define SOUND_BUFFERSIZE (2048 * SOUND_FREQ / 44100)
-#endif
-
-#ifndef SOUND_ENABLE_FX
-#define SOUND_ENABLE_FX 1
-#endif
-
-///////////////
-// image defines
-///////////////
-
-#ifndef STRICT_IMAGES
-#define STRICT_IMAGES 0
-#endif
-
-#ifndef MAX_IMAGE_RECORDING
-#define MAX_IMAGE_RECORDING 2
-#endif
 
 ///////////////
 // language defines
@@ -140,14 +50,3 @@
 #define HARDFAULT_DEBUG_HANDLER 0
 #endif
 
-///////////////
-// GUI defines
-///////////////
-
-#ifndef GUI_ENABLE_POPUP
-#define GUI_ENABLE_POPUP 1
-#endif
-
-#ifndef HOME_MENU_NO_EXIT
-#define HOME_MENU_NO_EXIT 0
-#endif
